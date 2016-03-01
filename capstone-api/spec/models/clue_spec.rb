938b71315bee79c0a47ff4c3d664474b5d22cc65
Travis Smith <travissmith94@hotmail.com>
2016-03-01 13:08:58 -0500

    Created specs for missing attributes on clues

--- capstone-api/spec/models/clue_spec.rb
+++ capstone-api/spec/models/clue_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Clue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:each) do
+		@clue = Clue.new(hint: 'Test' , question: 'Test' , answer: 'Test')
+	end
+
+	it "should save a valid clue" do
+		expect(@clue).to be_valid
+	end
+
+	it "should not save a clue with missing required fields" do
+		required_fields = [:hint , :question, :answer]
+		required_fields.each do |field|
+			invalid_clue = @clue.clone
+			invalid_clue.send("#{field}=", nil)
+			expect(invalid_clue).to_not be_valid
+		end
+	end
 end
