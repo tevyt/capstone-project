351d5bb6938dd281fb5f552d338afcb5938da7a2
Travis Smith <travissmith94@hotmail.com>
2016-03-08 07:10:52 -0500

    Fixed indentation on controller

--- capstone-api/app/controllers/application_controller.rb
+++ capstone-api/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController <ActionController::API
-	before_action :check_user_exists, only: [:test]
-
-	def test
-		return :ok
-	end
-
-	protected
-		def check_user_exists
-			@currentUser = User.where(email: params[:email], password: params[:password]).take
-		end
+  before_action :check_user_exists, only: [:test]
+  
+  def test
+    return :ok
+  end
+  
+  protected
+  def check_user_exists
+    @currentUser = User.where(email: params[:email], password: params[:password]).take
+  end
 end
--- capstone-api/app/models/concerns/distance_manager.rb
+++ capstone-api/app/models/concerns/distance_manager.rb
@@ -1,11 +1,11 @@
 module DistanceManager
-	extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-	included do
-		Float.class_eval do
-			def to_radians
-				self.to_f * Math::PI/180
-			end 
-		end
-	end
+  included do
+    Float.class_eval do
+      def to_radians
+        self.to_f * Math::PI/180
+      end 
+    end
+  end
 end
