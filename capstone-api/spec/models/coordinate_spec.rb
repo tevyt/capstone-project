18ed3c9c6707d9d5de6f09533c28a01817a45695
Travis Smith <travissmith94@hotmail.com>
2016-05-10 11:57:06 -0500

    Removed coordinate spec and coordinate model

--- capstone-api/spec/models/coordinate_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Coordinate, type: :model do
-	before (:each) do
-		@coordinate = Coordinate.new(latitude: 18.003 , longitude: -76.7446) #This is on UWI campus
-	end
-	
-
-end
